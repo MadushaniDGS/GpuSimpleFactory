a7c406ca9777f91672396bf63412601f2b9f6739
Madushani <Madushani@DESKTOP-JV13050>
2024-08-06 16:26:46 +0530

    Add project files.

---
+++ Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GpuSimpleFactory
+{
+    public abstract class Company
+    {
+        public Gpu AssempleGpu()
+        {
+            Gpu gpu = CreateGpu();
+            gpu.Assemble();
+            return gpu;
+        }
+        
+        public abstract Gpu CreateGpu();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace GpuSimpleFactory;
+
+public class Program
+{
+    public static void Main (string[] args)
+    {
+        MsiManufacture msiManufacture = new MsiManufacture();
+        msiManufacture.AssempleGpu();
+
+        AsusManufacture asusManufacture = new AsusManufacture();
+        asusManufacture.AssempleGpu();
+
+        Console.ReadLine();
+    }
+}
